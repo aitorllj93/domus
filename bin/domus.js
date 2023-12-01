f67b913048afc5a1e5a084f6be403d66b5e4d6d4
aitorllj93 <aitorllj93@gmail.com>
2023-12-01 21:29:30 +0100

    add more commands

--- bin/domus.js
+++ bin/domus.js
@@ -1,25 +1,33 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
-const { backup, processStatus, setup } = require("../lib/node/index.js");
+const { backup, processStatus, setup, stop } = require("../lib/node/index.js");
 const pkg = require("../package.json");
 const program = new Command();
 
 program.name("domus").description(pkg.description).version(pkg.version);
 
 program
-  .command("setup", { isDefault: true })
-  .description("Setup Domus")
+  .command("install", { isDefault: true })
+  .description("Install Domus")
   .option("--base-url <url>", "Domus base url")
   .option("-a, --apps <names>", "Apps to install")
   .option("-m, --mount-dir <dir>", "Mount directory")
   .option("-y, --yes", "Skip prompts")
   .action(setup);
-// program.command("install").description("Install Domus App").action(install);
+
+program
+  .command("stop")
+  .description("Stop Domus Apps")
+  .option("-a, --apps <names>", "Apps to install")
+  .option("-y, --yes", "Skip prompts")
+  .action(stop);
+
 program
   .command("status")
   .alias("ps")
   .description("Show Domus status")
   .action(processStatus);
+
 program
   .command("backup")
   .alias("bk")
